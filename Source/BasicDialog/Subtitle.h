9eae6e06f29858aeb9629d8ea881c96c58292419
Batname <dadubinin@gmail.com>
2017-06-25 13:58:24 +0200

    fix subtitles

--- Source/BasicDialog/Subtitle.h
+++ Source/BasicDialog/Subtitle.h
@@ -22,14 +22,13 @@
 USTRUCT(BlueprintType)
 struct FSubtitle : public FTableRowBase
 {
-	GENERATED_USTRUCT_BODY();
+	GENERATED_USTRUCT_BODY()
 
-
-	/** the subtitle that will displayed for a specific period of the tile in UI */
+	/*The subtitle that will be displayed for a specific period of time in our UI*/
 	UPROPERTY(EditAnywhere)
 	FString Subtitle;
 
-	/** The relative time in seconds, that subtitle will appear */
+	/*The relative time in seconds, that the subtitle will appear*/
 	UPROPERTY(EditAnywhere)
 	float AssociatedTime;
 };
