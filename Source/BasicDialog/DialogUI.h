46eb457299faf9160614002cb4d9b05072bc4acb
Batname <dadubinin@gmail.com>
2017-06-25 13:26:23 +0200

    finish coding

--- Source/BasicDialog/DialogUI.h
+++ Source/BasicDialog/DialogUI.h
@@ -30,3 +30,40 @@ public:
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = DialogSystem)
 	void Show();
 };
+
+
+class UMGAsyncTask : public FNonAbandonableTask
+{
+	TArray<FSubtitle> Subs;
+
+	UDialogUI* DialogUI;
+
+public:
+
+	UMGAsyncTask(TArray<FSubtitle>& Subs, UDialogUI* DialogUI)
+	{
+		this->Subs = Subs;
+		this->DialogUI = DialogUI;
+	}
+
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(UMGAsyncTask, STATGROUP_ThreadPoolAsyncTasks);
+	}
+
+	void DoWork()
+	{
+		for (int32 i = 0; i < Subs.Num(); i++)
+		{
+			//Sleep means that we pause this thread for the given time
+			FPlatformProcess::Sleep(Subs[i].AssociatedTime);
+
+			//Update our subtitles after the thread comes back
+			DialogUI->SubtitleToDisplay = Subs[i].Subtitle;
+		}
+
+		FPlatformProcess::Sleep(1.f);
+
+		DialogUI->SubtitleToDisplay = FString("");
+	}
+};
